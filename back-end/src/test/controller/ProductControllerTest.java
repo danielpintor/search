d91e40946f1f3ff0874dcabd9ad9fa1170990074
danielpintor <86312023+danielpintor@users.noreply.github.com>
2022-07-23 18:54:01 -0400

    Create ProductControllerTest.java

---
+++ back-end/src/test/controller/ProductControllerTest.java
@@ -0,0 +1,14 @@
+import static org.mockito.Mockito;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import java.util.Arrays;
+import java.util.List;
+
+import src.main.data.products;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ProductControllerTest {
+	
+	
+}
